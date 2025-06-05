f85c3f173b65af8ddd48118a129a5a0faa2a3f8f
Muhammad Rizki Duwinanto Muhammad.Rizki.Duwinanto@student.rug.nl <rizkiduwinanto@gmail.com>
2025-06-05 12:53:27 +0200

    add slurm script

--- src/jobscript-experiment.sh
+++ src/jobscript-experiment.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 #SBATCH --job-name=Forest-experiment
 #SBATCH --output=Forest-experiment_%j_%a.out
-#SBATCH --time=24:00:00
+#SBATCH --time=72:00:00
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=16GB
 #SBATCH --partition=gpu
 #SBATCH --cpus-per-task=6
-#SBATCH --array=1-12
+#SBATCH --array=1-13
 
 module load CUDA/12.4.0
 source /home4/$USER/venvs/umcg_env/bin/activate
 
 mkdir -p $TMPDIR/results
+mkdir -p $TMPDIR/models
 cp -R /home4/$USER/Survival-Tree-Analysis $TMPDIR/Survival-Tree-Analysis
 cd $TMPDIR/Survival-Tree-Analysis
+
+trap 'mkdir -p /home4/$USER/job_${SLURM_JOBID}
+tar czvf /home4/$USER/job_${SLURM_JOBID}/models.tar.gz $TMPDIR/models
+cp $TMPDIR/results/${OUTPUT_FILE} /home4/$USER/job_${SLURM_JOBID}/' 12
 
 CONFIGURATIONS=(
     "extreme --no-is_custom_dist --no-is_bootstrap"
@@ -43,17 +48,21 @@ CMD="python3 src/main_experiment.py \
     --dataset=\"support\" \
     --function=\"$FUNCTION\" \
     --no-is_grid \
-    --no-is_cv \
+    --is_cv \
     --n_tries=10 \
     --n_models=5 \
+    --n_splits=5 \
+    --path=\"$TMPDIR/models\" \
     --path-res=\"$TMPDIR/results/${OUTPUT_FILE}\" \
+    --aggregator=\"median\" \
+    
     $FLAGS"
 
 echo "Running configuration $SLURM_ARRAY_TASK_ID: $FUNCTION with $FLAGS"
 echo "Command: $CMD"
-eval $CMD
+eval $CMD &
+wait
 
 mkdir -p /home4/$USER/job_${SLURM_JOBID}
 tar czvf /home4/$USER/job_${SLURM_JOBID}/models.tar.gz $TMPDIR/models
-cp $TMPDIR/${OUTPUT_FILE} /home4/$USER/job_${SLURM_JOBID}/results_${PARAM}_${SLURM_ARRAY_TASK_ID}.csv
-
+cp $TMPDIR/results/${OUTPUT_FILE} /home4/$USER/job_${SLURM_JOBID}/
