34915d3a654406c3246ec9d1643828e6755591d2
rizkiduwinanto <rizkiduwinanto@gmail.com>
2025-06-24 18:34:51 +0200

    script for RSF

--- src/script/jobscript-experiment-rsf.sh
+++ src/script/jobscript-experiment-rsf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=RSForest-experiment-support
 #SBATCH --output=RSForest-experiment-support.out
-#SBATCH --time=24:00:00
+#SBATCH --time=01:00:00
 #SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=24GB
 
@@ -12,13 +12,18 @@ export WANDB_API_KEY=<WANDB_API_KEY>
 
 wandb login
 
+DATASETS=("support" "nhanes")
+DATASET=${DATASETS[$SLURM_ARRAY_TASK_ID-1]}
+
+OUTPUT_FILE="results_rsf_${DATASET}.csv"
+
 mkdir -p $TMPDIR/results
 cp -R /home4/$USER/Survival-Tree-Analysis $TMPDIR/Survival-Tree-Analysis
 cd $TMPDIR/Survival-Tree-Analysis
 
-trap 'mkdir -p /home4/$USER/job_${SLURM_JOBID}
-tar czvf /home4/$USER/job_${SLURM_JOBID}/models.tar.gz $TMPDIR/models
-cp $TMPDIR/results/${OUTPUT_FILE} /home4/$USER/job_${SLURM_JOBID}/' 12
+trap 'mkdir -p /home4/$USER/job_${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID};
+tar czvf /home4/$USER/job_${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID}/models.tar.gz $TMPDIR/results/models;
+cp $TMPDIR/results/${OUTPUT_FILE} /home4/$USER/job_${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID}/' 12
 
 CMD="python3 src/main_experiment.py \
     --parameter=\"randomsurvivalforest\" \
@@ -28,7 +33,7 @@ CMD="python3 src/main_experiment.py \
     --n_tries=10 \
     --n_models=5 \
     --n_splits=5 \
-    --path=\"$TMPDIR/results/models\" \
+    --path=\"$TMPDIR/results/models/model\" \
     --path-res=\"$TMPDIR/results/${OUTPUT_FILE}\" \
     "
 
